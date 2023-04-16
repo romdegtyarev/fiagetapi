be8803878d32f1335949eabe6e7ca3aa4c5f251e
Roman Degtyarev <guromka@gmail.com>
2023-04-16 19:23:20 +0700

    Refactor and bugfix.

--- Dockerfile
+++ Dockerfile
@@ -9,18 +9,18 @@ RUN apt-get update && apt-get install -y \
     python3 \
     python3-venv \
     python-pip \
-    tzdata \
+    poppler-utils \
+    python-poppler \
+    libpoppler-cpp-dev \
     vim \
     sudo
 
 RUN ln -fs /ust/share/zoneinfo/Asia/Novosibirsk /etc/localtime
-RUN dpkg-reconfigure --frontend noninteractive tzdata
 
 RUN groupadd -g $GROUP_ID $USER_GROUP_NAME
 RUN useradd -u $USER_ID -g $GROUP_ID -m $USER_GROUP_NAME
 
 RUN mkdir -p /home/fia/pyvenv
-RUN cd
 RUN python3 -m venv /home/fia/pyvenv
 RUN /home/fia/pyvenv/bin/python3 -m pip install --upgrade pip
 RUN /home/fia/pyvenv/bin/pip3 install schedule
@@ -31,5 +31,4 @@ RUN /home/fia/pyvenv/bin/pip3 install requests
 RUN mkdir -p /home/fia/fia
 RUN chown -R $USER_GROUP_NAME:$USER_GROUP_NAME /home/fia/fia
 RUN chmod -R 777 /home/fia/fia
-
 
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 ################################################################################
-# Imports
 ################################################################################
 import requests
 import datetime
@@ -15,18 +14,21 @@ import config
 # Main variables
 ################################################################################
 FIA_URL = "https://www.fia.com/"
-FIA_URL_DOCS = "https://www.fia.com/documents/championships/fia-formula-one-world-championship-14/season/season-2022-2005"
+FIA_URL_DOCS = "https://www.fia.com/documents/season/season-2023-2042/championships/fia-formula-one-world-championship-14"
 RACE_TEMPLATE = "Final Race Classification"
+GRID_TEMPLATE = "Final Starting Grid"
 
 TELEGRAM_BOT_API = "https://api.telegram.org/bot"
-TELEGRAM_BOT_API_SEND_PHOTO_METHOD = "sendPhoto?"
+TELEGRAM_BOT_API_SEND_PHOTO_METHOD = "/sendPhoto?"
 
 # Config
-DATABASE_NAME = "local_database"
-SCHEDULED_TASK_DELAY = 240 # Sec
-SCHEDULED_TASK_SYNCHRONIZATION_INTERVAL = 60
+DATABASE_PATH = "/home/fia/fia/fia_get/db/"
+DATABASE_NAME = "/home/fia/fia/fia_get/local_database"
+SCHEDULED_TASK_DELAY = 60  # Sec
+SCHEDULED_TASK_SYNCHRONIZATION_INTERVAL = 240  # Min
 GROUP_CHAT_ID = config.chat_id
 TOKEN = config.token
+
 
 ################################################################################
 # Functions
@@ -36,58 +38,92 @@ TOKEN = config.token
 # description: 
 ################################################################################
 def send_photo(file_name, chat_id):
+    """
+
+    :param file_name:
+    :param chat_id:
+    """
     image = open(file_name, "rb")
+    # TODO: Check
     url = TELEGRAM_BOT_API + TOKEN + TELEGRAM_BOT_API_SEND_PHOTO_METHOD + "chat_id=" + chat_id
-    #r = requests.post(url, files={'photo': image})
-    print(url)
+    r = requests.post(url, files={'photo': image})
+    print("send_photo: result: ", r.text)
+
 
 ################################################################################
 # name:        
 # description: 
 ################################################################################
 def convert_save_send_image(in_file_name, out_file_name, page):
-    print(in_file_name, out_file_name, page)
+    """
+
+    :param in_file_name:
+    :param out_file_name:
+    :param page:
+    """
     images = convert_from_path(in_file_name)
-    image_name = out_file_name + '.jpg'
+    image_name = DATABASE_PATH + out_file_name + '.jpg'
     images[page].save(image_name, 'JPEG')
     send_photo(image_name, GROUP_CHAT_ID)
+
 
 ################################################################################
 # name:        
 # description: 
 ################################################################################
 def download_pdf(url, file_name, headers):
-    print(url, file_name)
+    """
+
+    :param url:
+    :param file_name:
+    :param headers:
+    """
     # Send GET request
-    response = requests.get(url, headers = headers)
+    response = requests.get(url, headers=headers)
     # Save the PDF
     if response.status_code == 200:
         with open(file_name, "wb") as f:
             f.write(response.content)
     else:
-        print(response.status_code)
+        print("download_pdf: ", response.status_code)
+
 
 ################################################################################
 # name:        
 # description: 
 ################################################################################
 def add_to_local_database(site_dates, site_links):
+    """
+
+    :param site_dates:
+    :param site_links:
+    """
     pdf_url = FIA_URL + site_links
-    pdf_file_name = site_dates + ".pdf"
+    pdf_file_name = DATABASE_PATH + site_dates + ".pdf"
     download_pdf(pdf_url, pdf_file_name, "")
+    # TODO: Result
 
     local_database = open(DATABASE_NAME, "a")
+    # TODO: Check result
     local_database.write(str(site_dates) + "\n")
     local_database.close()
 
     convert_save_send_image(pdf_file_name, site_dates, 1)
+
 
 ################################################################################
 # name:        
 # description: 
 ################################################################################
 def cmp_with_local_database(site_dates, site_links):
+    """
+
+    :param site_dates:
+    :param site_links:
+    """
+    # TODO: Check site_dates len
     local_database = open(DATABASE_NAME, "r")
+    # TODO: Check if opened
     local_database_dates = []
     while True:
         line = local_database.readline()
@@ -98,7 +134,10 @@ def cmp_with_local_database(site_dates, site_links):
 
     # Get last date
     format = "%d.%m.%y %H:%M"
-    local_database_date_str = local_database_dates[len(local_database_dates)-1]
+    count = len(local_database_dates)
+    local_database_date_str = "01.01.01 00:00"
+    if count > 0:
+        local_database_date_str = local_database_dates[len(local_database_dates) - 1]
     local_database_date_object = datetime.datetime.strptime(local_database_date_str, format)
 
     site_dates.reverse()
@@ -111,13 +150,18 @@ def cmp_with_local_database(site_dates, site_links):
             print("cmp_with_local_database: add_to_local_database")
             add_to_local_database(site_database_date_str, site_database_link_str)
 
+
 ################################################################################
 # name:        update_info_from_site
 # description: 
 ################################################################################
 def update_info_from_site():
+    """
+    Update info from site
+    """
     print("update_info_from_site: start")
     url = FIA_URL_DOCS
+    # TODO: Check response
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     docs = soup.find_all("li", "document-row")
@@ -126,7 +170,8 @@ def update_info_from_site():
     result_dates = []
     for i in docs:
         if not RACE_TEMPLATE in i.text:
-            continue
+            if GRID_TEMPLATE not in i.text:
+                continue
         docs_race_link_with_tags = i.find("a")
         docs_race_link = docs_race_link_with_tags.get("href")
 
@@ -139,13 +184,18 @@ def update_info_from_site():
         result_links.append(docs_race_link_str)
         result_dates.append(docs_race_date_str)
 
+    # print("update_info_from_site: dates: ", result_dates, "links: ", result_links)
     cmp_with_local_database(result_dates, result_links)
+
 
 ################################################################################
 # name:        scheduled_task
 # description: Task for scheduled routine
 ################################################################################
 def scheduled_task():
+    """
+    Scheduled task
+    """
     schedule.every(SCHEDULED_TASK_SYNCHRONIZATION_INTERVAL).minutes.do(update_info_from_site)
     print("scheduled_task: Start")
     while True:
@@ -153,13 +203,17 @@ def scheduled_task():
         schedule.run_pending()
         time.sleep(SCHEDULED_TASK_DELAY)
 
+
 ################################################################################
 # Main
 ################################################################################
 def main():
+    """
+    Main
+    """
+    print("Starting FIA GET BOT")
     scheduled_task()
+
 
 if __name__ == "__main__":
     main()
-
-
